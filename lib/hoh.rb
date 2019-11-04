43f59e15eacf9c2c726f7eb1b91dd5f727affdf4
Baruch Phillips <baruchphillips@gmail.com>
2019-11-04 18:54:33 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,22 +48,20 @@ naming_system = {
       :sub_category => {
         label: 'Class',
         :sub_category => {
-          label: 'Order'
+          label: 'Order' 
+           :sub_category => {
+             label: 'Family' 
+              :sub_category => {
+                label: 'Genus' 
+                  :sub_category => {
+                    label: 'Species' 
+                     :sub_category => {
+                       label: nil 
+                     }
+                  }
+              }
+           }
         }
-                       }
-                      }
- }
-  {
-  label: 'Order'
-  }
- {
-  label: 'family',
-     :sub_category => {
-       label: 'Genus'
-                      }
-  }
-   {
-  label: 'Species',
-   :sub_category => nil
-    }
+      }}}               
+                    
 end
