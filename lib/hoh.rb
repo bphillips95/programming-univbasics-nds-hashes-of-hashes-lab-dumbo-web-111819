178ba2c86ab74a343b6452824bc7743d53b067b0
Baruch Phillips <baruchphillips@gmail.com>
2019-11-04 18:20:20 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,26 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-
+naming_system = {
+  label: "kingdom",
+    :sub_category => {
+      label: 'phylum',
+      :sub_category => {
+        label: 'class'
+                       }
+                      }
+                  }
+  {
+  label: 'Order'
+  }
+ {
+  label: 'family',
+     :sub_category => {
+       label: 'Genus'
+                      }
+  }
+   {
+  label: 'Species',
+   :sub_category => nil
+    }
 end
